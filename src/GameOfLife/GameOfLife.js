79e28493629bddc5ffecd25c1c8a99e7bfbea9fb
Jeff Simon <jeffsimon22@yahoo.com>
2019-08-01 19:09:12 -0700

    modified files

--- src/GameOfLife/GameOfLife.js
+++ src/GameOfLife/GameOfLife.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './GameOfLife.css';
 
 import GameOfLifeGrid from "./GameOfLifeGrid";
+import GameControls from "./GameControls";
 
 class GameOfLife extends Component {
     constructor(props){
@@ -37,107 +38,12 @@ class GameOfLife extends Component {
             start: startingGrid,
             grid: startingGrid,
             edit: editingGrid,
-            isEditing: false,
-            isPlaying: false,
-            playInterval: null
+            isEditing: false
         };
     }
 
-    getLiveNeighbors = (row, col) => {
-        let neighbors = 0;
-        [row-1, row, row+1].forEach((r) => {
-            if ((r >= 0) && (r < this.state.rows)) {
-                [col-1, col, col+1].forEach((c) => {
-                    if ((c >= 0) && 
-                        (c < this.state.cols) &&
-                        !(r === row && c === col)) {
-                        if (this.state.grid[r][c]) {
-                            neighbors++;
-                        }
-                    }
-                }); 
-            }
-        });
-        return neighbors;
-    }
-
-    nextGen = () => {
-        let next = this.state.grid.map(r => r.slice());
-
-        for (let r=0; r<this.state.rows; r++) {
-            for (let c=0; c<this.state.cols; c++) {
-                const aliveNeighbors = this.getLiveNeighbors(r,c);
-                if (this.state.grid[r][c]) {
-                    if (aliveNeighbors < 2) {
-                        next[r][c] = false;
-                    } else if (aliveNeighbors === 2 || aliveNeighbors === 3) {
-                        next[r][c] = true;
-                    } else {
-                        next[r][c] = false;
-                    }
-                } else if (aliveNeighbors === 3) {
-                    next[r][c] = true;
-                }
-            }
-        }
-        this.setState({grid: next});
-    }
-
-    playGame = () => {
-        let interval = null;
-        if (this.state.isPlaying) {
-            clearInterval(this.state.playInterval);
-        } else {
-            interval = setInterval(() => this.nextGen(), 1000);
-        }
-        this.setState(prev => { 
-            return { 
-                isPlaying: !prev.isPlaying,
-                playInterval: interval
-            }
-        });
-    }
-
-    resetGame = () => {
-        this.setState({
-            grid: this.state.start.map(r => r.slice()),
-            isEditing: false
-        });
-    }
-
-    handleCellClick = (row, col) => {
-        // if not in edit mode, don't do anything
-        if (!this.state.isEditing) {
-            return;
-        }
-
-        let edit = this.state.edit.map(r => r.slice());
-        edit[row][col] = !edit[row][col];
-        this.setState({
-            edit: edit.slice(),
-            grid: edit.slice()
-        });
-    }
-
-    editStart = () => {
-        let reset = this.state.grid.map(r => r.slice()).map(r => {
-            return r.map(v => false);
-        });
-        this.setState({
-            grid: reset.slice(),
-            edit: reset.slice(),
-            isEditing: true
-        });
-    }
-
-    setStart = () => {
-        let edit = this.state.edit.map(r => r.slice());
-        this.setState({
-            grid: edit.slice(),
-            edit: edit.slice(),
-            start: edit.slice(),
-            isEditing: false
-        });
+    updateState = (s) => {
+        this.setState(s);
     }
 
     render() {
@@ -145,26 +51,16 @@ class GameOfLife extends Component {
             <div className="GameOfLife">
                 <h2>Game of Life</h2>
                 <main>
-                    <section className="controls">
-                        <div>
-                            <button onClick={this.playGame}>
-                                { this.state.isPlaying ? "Stop" : "Play" }
-                            </button>
-                        </div>
-                        <button onClick={this.resetGame}>Reset</button>
-                        
-                        {
-                            this.state.isEditing ?
-                            <button onClick={this.setStart}>Set Start</button> :
-                            <button onClick={this.editStart}>Edit Start</button>
-                        }
-                        
+                    <section>
+                        <GameControls 
+                            gameState={this.state} 
+                            update={this.updateState}
+                        />
                     </section>
                     <section >
                         <GameOfLifeGrid 
-                            grid={this.state.grid}
-                            handleCellClick={this.handleCellClick}
-                            isEditing={this.state.isEditing}
+                            gameState={this.state}
+                            update={this.updateState}
                         />
                     </section>
                 </main>
--- src/GameOfLife/GameOfLifeGrid.js
+++ src/GameOfLife/GameOfLifeGrid.js
@@ -1,29 +1,50 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-function GameOfLifeGrid(props) {
-	const rows = props.grid.map((row, i) => {
-        const cells = row.map((status, j) => {
-            const classnames = status ? ["alive"] : ["dead"];
-            if (props.isEditing) {
-            	classnames.push("editMode");
-            }
-            return (<td key={j} 
-                className={classnames.join(" ")} 
-                onClick={() => props.handleCellClick(i,j)}>
-                    {i},{j}
-                </td> 
-            )
+class GameOfLifeGrid extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    handleCellClick = (row, col) => {
+        // if not in edit mode, don't do anything
+        if (!this.props.gameState.isEditing) {
+            return;
+        }
+
+        let edit = this.props.gameState.edit.map(r => r.slice());
+        edit[row][col] = !edit[row][col];
+        this.props.update({
+            edit: edit.slice(),
+            grid: edit.slice()
         });
-        return <tr key={i}>{cells}</tr>
-    });
+    }
 
-	return (
-		<table>
-            <tbody>
-                {rows}
-            </tbody>
-        </table>
-	)
+    render() {
+
+    	const rows = this.props.gameState.grid.map((row, i) => {
+            const cells = row.map((status, j) => {
+                const classnames = status ? ["alive"] : ["dead"];
+                if (this.props.gameState.isEditing) {
+                	classnames.push("editMode");
+                }
+                return (<td key={j} 
+                    className={classnames.join(" ")} 
+                    onClick={() => this.handleCellClick(i,j)}>
+                        {i},{j}
+                    </td> 
+                )
+            });
+            return <tr key={i}>{cells}</tr>
+        });
+
+    	return (
+    		<table>
+                <tbody>
+                    {rows}
+                </tbody>
+            </table>
+    	)
+    }
 }
 
 export default GameOfLifeGrid;
